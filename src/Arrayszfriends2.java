f338ae76582c8bf82721654d8108408af74948cb
Emil Isabekov <Emil.Isabekov@sidenis.com>
2020-11-29 23:17:27 +0700

    new class

---
+++ src/Arrayszfriends2.java
@@ -0,0 +1,21 @@
+public class Arrayszfriends2 {
+    public static void main (String[] args) {
+        String[] friends = new String[20];
+        friends[0] = "Sergey1";
+        friends[1] = "Sergey2";
+        friends[2] = "Sergey3";
+        friends[3] = "Sergey4";
+        friends[19] = "Sergey5";
+        int i;
+        int totalElements = friends.length;
+
+        for (i = 0; i < totalElements; i++){
+            if (friends[i] == "Sergey3")
+                continue;
+            System.out.println("I want to meet with " + friends[i]);
+//            i++;
+        }
+        System.out.println("И тут я встретил его");
+    }
+}
+
