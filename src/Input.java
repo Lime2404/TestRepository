a7bc2d85defbed0037af2d5cbd37d2eef0bcb840
Emil Isabekov <Emil.Isabekov@sidenis.com>
2021-01-08 23:22:20 +0700

    input validation

--- src/Input.java
+++ src/Input.java
@@ -3,8 +3,21 @@ import java.util.*;
 public class Input {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        System.out.println("введите ваши пароль");
-        String userName = obj.nextLine();
-        System.out.println("подтверждаю " + userName);
+        System.out.println("введите цифорный пароль");
+
+        if (obj.hasNextInt()) {
+            String userName = obj.nextLine();
+            System.out.println("подтверждаю " + userName);
+
+        } else
+        System.out.println("вы введи буквы, попробуйте ещё раз ");
+
+        Scanner obj1 = new Scanner(System.in);
+        System.out.println("введите цифорный пароль");
+        while (obj1.hasNextInt()) {
+            String userName = obj1.nextLine();
+            System.out.println("подтверждаю " + userName);
+        }
+        System.out.println("вы первысили максимальное количество попыток ");
     }
 }
