507cce88c136df006f7d912203eeb1375ee9a13f
Lime2404 <772433>
2021-03-11 11:55:01 +0700

    cycle and scanner

--- src/Displayit.java
+++ src/Displayit.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Displayit {
     public static void main(String[] args) {
-        Scanner source = new Scanner(System.in);
-        String sentence = source.nextLine();
-        System.out.println(sentence);
-
+        boolean isExit = false;
+        while (!isExit) {
+            Scanner source = new Scanner(System.in);
+            String sentence = source.nextLine();
+            System.out.println(sentence);
+            isExit = sentence.equals("exit");
+        }
     }
 }
