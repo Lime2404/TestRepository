cc2aa5c6ba821077007b1f9d8fd3da2866271b74
Lime2404 <772433>
2021-03-11 11:13:45 +0700

    cycle

---
+++ src/Cycle.java
@@ -0,0 +1,9 @@
+public class Cycle {
+    public static void main(String[] args) {
+        int i = 3;
+        while (i >= 0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
