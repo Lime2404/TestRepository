a764787e72e13483d21f1e0592021fdc859cfb22
Lime2404 <772433>
2021-05-21 18:10:27 +0700

    For_cycle

--- src/Cat.java
+++ src/Cat.java
@@ -9,6 +9,13 @@ public class Cat {
     public static void main(String[] args){
             Cat murzik = new Cat("Мурзик", 5);
             System.out.println(murzik.name);
+            int e = murzik.hashCode();
+        System.out.println(e);
+        Cat Misty = new Cat("Мистие", 1);
+        System.out.println(Misty.name);
+        int m = Misty.hashCode();
+        System.out.println(m);
+
         }
 }
 
