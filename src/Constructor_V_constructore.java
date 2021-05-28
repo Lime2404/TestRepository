33728a24cd7ffa55a71f7ac67c696b098d7b33c9
Lime2404 <772433>
2021-05-28 22:25:16 +0700

    constructor

---
+++ src/Constructor_V_constructore.java
@@ -0,0 +1,33 @@
+public class Constructor_V_constructore {
+
+
+        public double x;
+        public double y;
+        public double radius;
+
+        public Constructor_V_constructore(double x, double y, double radius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public Constructor_V_constructore(double x, double y) {
+            //напишите тут ваш код
+//        this(x, y);
+//        this.radius = null;
+            this(x, y, 10);
+
+        }
+
+        public Constructor_V_constructore() {
+            this(5, 5, 1);
+        }
+
+        public static void main(String[] args) {
+            Constructor_V_constructore circle = new Constructor_V_constructore();
+            System.out.println(circle.x + " " + circle.y + " " + circle.radius);
+            Constructor_V_constructore anotherCircle = new Constructor_V_constructore(10, 5);
+            System.out.println(anotherCircle.x + " " + anotherCircle.y + " " + anotherCircle.radius);
+        }
+
+}
