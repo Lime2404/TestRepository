75699bcdadf83189236564090622efcefe37760a
Lime2404 <772433>
2021-06-24 22:45:37 +0700

    Array

---
+++ src/SimpleArray.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class SimpleArray {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Vodlvo");
+        cars.add("Niva");
+        cars.add("Lada");
+        System.out.println(cars);
+    }
+}
