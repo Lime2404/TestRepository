08db9a34fedc52d5ba70cd0b8ecc3167f911068d
Lime2404 <772433>
2021-06-27 20:06:59 +0700

    Array

---
+++ src/ArrayInput.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ArrayInput {
+//    public class MainClass
+
+        public static void main(String[] args) throws IOException
+        {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String[] list = new String[4];
+
+            for (int i = 0; i < list.length; i++)
+            {
+                list[i] = reader.readLine();
+            }
+            System.out.println(list[2]);
+        }
+}
